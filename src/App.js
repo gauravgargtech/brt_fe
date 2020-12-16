5fd8841d8f618c93a9cf277417654d3c33ea2577
Gaurav Garg <gauravgargtech2@gmail.com>
2020-12-16 13:53:51 +1100

    .gitignore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/home";
+import Create from "./components/create";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Route path="/create" exact component={Create}></Route>
+          <Route path="/" exact component={Home}></Route>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/create.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Create(props) {
+  return (
+    <div>
+        This is home
+    </div>
+  );
+}
+
+export default Create;
